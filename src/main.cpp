07d7a0ad907ae02d1802fab6706198eb00450409
paudresaire <pau.dresaire@estudiantat.upc.edu>
2023-05-09 17:10:49 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+const int potPin = 34;
+int potValue = 0;
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+}
+
+void loop() {
+    potValue = analogRead(potPin);
+    Serial.println(potValue);
+    delay(500);
+}
